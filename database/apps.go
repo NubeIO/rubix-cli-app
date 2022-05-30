f0c2fb5e6abde650eeeed9099b9ada767091ab45
NubeDev <aidan@enviroservices.com.au>
2022-05-30 12:14:29 +1000

    added store

--- database/apps.go
+++ database/apps.go
@@ -1,17 +1,15 @@
 package dbase
 
 import (
-	"encoding/json"
 	"errors"
 	"fmt"
 	"gthub.com/NubeIO/rubix-cli-app/pkg/helpers/uuid"
 	"gthub.com/NubeIO/rubix-cli-app/pkg/logger"
 	"gthub.com/NubeIO/rubix-cli-app/service/apps"
-	"gthub.com/NubeIO/rubix-cli-app/service/product"
 )
 
-func (d *DB) GetApps() ([]*apps.Store, error) {
-	var m []*apps.Store
+func (d *DB) GetApps() ([]*apps.InstalledApp, error) {
+	var m []*apps.InstalledApp
 	if err := d.DB.Find(&m).Error; err != nil {
 		return nil, err
 	} else {
@@ -19,8 +17,8 @@ func (d *DB) GetApps() ([]*apps.Store, error) {
 	}
 }
 
-func (d *DB) GetApp(uuid string) (*apps.Store, error) {
-	var m *apps.Store
+func (d *DB) GetApp(uuid string) (*apps.InstalledApp, error) {
+	var m *apps.InstalledApp
 	if err := d.DB.Where("uuid = ? ", uuid).First(&m).Error; err != nil {
 		logger.Errorf("GetApp error: %v", err)
 		return nil, err
@@ -28,8 +26,8 @@ func (d *DB) GetApp(uuid string) (*apps.Store, error) {
 	return m, nil
 }
 
-func (d *DB) GetAppByName(name string) (*apps.Store, error) {
-	var m *apps.Store
+func (d *DB) GetAppByName(name string) (*apps.InstalledApp, error) {
+	var m *apps.InstalledApp
 	if err := d.DB.Where("name = ? ", name).First(&m).Error; err != nil {
 		logger.Errorf("GetApp error: %v", err)
 		return nil, err
@@ -37,89 +35,14 @@ func (d *DB) GetAppByName(name string) (*apps.Store, error) {
 	return m, nil
 }
 
-func checkProduct(products []string) error {
-	if len(products) == 0 {
-		return errors.New("product list can not be empty try, RubixCompute, Edge28")
-	}
-	for _, pro := range products {
-		_, err := product.CheckProduct(pro)
-		if err != nil {
-			return err
-		}
-	}
-	return nil
-}
-
-const (
-	Owner   = "NubeIO"
-	User    = "root"
-	TempDir = "/tmp"
-)
-
-func validateAllowableProducts(store *apps.Store) ([]byte, []string, error) {
-	if store.AllowableProducts == nil {
-		return nil, nil, nil
-	}
-	var data []string
-	err := json.Unmarshal(store.AllowableProducts, &data)
+func (d *DB) AddApp(body *apps.InstalledApp) (*apps.InstalledApp, error) {
+	store, err := d.GetAppImageByName(body.AppStoreName)
 	if err != nil {
-		return nil, nil, err
+		return nil, errors.New("no app store is installed for this app")
 	}
-	valid, err := json.Marshal(data)
-	if err != nil {
-		return nil, nil, err
-	}
-	return valid, data, nil
-}
-
-func (d *DB) CreateApp(body *apps.Store) (*apps.Store, error) {
 	body.UUID = fmt.Sprintf("app_%s", uuid.SmallUUID())
-	pro, err := product.Get()
-	appType, appTypeName, err := apps.CheckType(body.AppTypeName)
-	if err != nil {
-		return nil, err
-	}
-	products, productsList, err := validateAllowableProducts(body)
-	if err != nil {
-		return nil, err
-	}
-	err = checkProduct(productsList)
-	if err != nil {
-	}
-
-	if body.RubixRootPath == "" {
-		body.RubixRootPath = "/data"
-	}
-	if body.AppsPath == "" {
-		body.AppsPath = "/data/rubix-apps/installed"
-	}
-
-	if body.Owner == "" {
-		body.Owner = Owner
-	}
-	if body.RunAsUser == "" {
-		body.RunAsUser = User
-	}
-	if body.DownloadPath == "" {
-		body.DownloadPath = TempDir
-	}
-
-	body.AllowableProducts = products
-	body.AppTypeName = appTypeName
-	body.AppType = appType
-	get, err := product.Get()
-	if err != nil {
-		return nil, err
-	}
-	body.Arch = get.Arch
-	if err != nil {
-		return nil, err
-	}
-	body.ProductType = pro.Type
-	if err != nil {
-		return nil, err
-	}
-	fmt.Println(body)
+	body.AppStoreUUID = store.UUID
+	body.AppStoreName = store.Name
 	if err := d.DB.Create(&body).Error; err != nil {
 		return nil, err
 	} else {
@@ -127,8 +50,8 @@ func (d *DB) CreateApp(body *apps.Store) (*apps.Store, error) {
 	}
 }
 
-func (d *DB) UpdateApp(uuid string, app *apps.Store) (*apps.Store, error) {
-	var m *apps.Store
+func (d *DB) UpdateApp(uuid string, app *apps.InstalledApp) (*apps.InstalledApp, error) {
+	var m *apps.InstalledApp
 	query := d.DB.Where("uuid = ?", uuid).Find(&m).Updates(app)
 	if query.Error != nil {
 		return nil, query.Error
@@ -138,18 +61,13 @@ func (d *DB) UpdateApp(uuid string, app *apps.Store) (*apps.Store, error) {
 }
 
 func (d *DB) DeleteApp(uuid string) (*DeleteMessage, error) {
-	var m *apps.Store
+	var m *apps.InstalledApp
 	query := d.DB.Where("uuid = ? ", uuid).Delete(&m)
 	return deleteResponse(query)
 }
 
-type DeleteMessage struct {
-	Message string `json:"message"`
-}
-
-// DropApps delete all.
 func (d *DB) DropApps() (*DeleteMessage, error) {
-	var m *apps.Store
+	var m *apps.InstalledApp
 	query := d.DB.Where("1 = 1")
 	query.Delete(&m)
 	return deleteResponse(query)
