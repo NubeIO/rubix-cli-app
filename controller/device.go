0a5acaca179fa8cd5c8b5b0e580d985452a80d77
NubeDev <aidan@enviroservices.com.au>
2022-08-17 17:08:22 +1000

    added network info to public

--- controller/device.go
+++ controller/device.go
@@ -1,33 +1,9 @@
 package controller
 
 import (
-	"github.com/NubeIO/lib-rubix-installer/installer"
 	"github.com/NubeIO/rubix-edge/pkg/model"
 	"github.com/gin-gonic/gin"
 )
-
-type DeviceProduct struct {
-	Device  *model.DeviceInfo  `json:"device"`
-	Product *installer.Product `json:"product"`
-}
-
-func (inst *Controller) GetDeviceProduct(c *gin.Context) {
-	device, err := inst.DB.GetDeviceInfo()
-	if err != nil {
-		reposeHandler(device, err, c)
-		return
-	}
-	product, err := inst.Rubix.App.GetProduct() // https://github.com/NubeIO/lib-command/blob/master/product/product.go#L7
-	if err != nil {
-		reposeHandler(device, err, c)
-		return
-	}
-	deviceProduct := &DeviceProduct{
-		Device:  device,
-		Product: product,
-	}
-	reposeHandler(deviceProduct, err, c)
-}
 
 func (inst *Controller) GetDeviceInfo(c *gin.Context) {
 	data, err := inst.DB.GetDeviceInfo()
